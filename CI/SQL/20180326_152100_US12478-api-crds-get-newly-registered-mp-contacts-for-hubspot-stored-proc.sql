12b9fdb2d4161aa64292ddd3392002b0ceca2b65
agause <agause@callibrity.com>
2018-04-13 15:32:15 -0400

    - 11th hour revelations around how to find "registered" contacts. Must join through dbo.dp_Users to be sure we're doing the right thing in prod.

--- CI/SQL/20180326_152100_US12478-api-crds-get-newly-registered-mp-contacts-for-hubspot-stored-proc.sql
+++ CI/SQL/20180326_152100_US12478-api-crds-get-newly-registered-mp-contacts-for-hubspot-stored-proc.sql
@@ -9,16 +9,18 @@ as
     select              Contacts.Contact_ID as MinistryPlatformContactId,
                         Contacts.Nickname as Firstname,
                         Contacts.Last_Name as Lastname,
-                        Contacts.Email_Address as Email,
+                        dp_Users.User_Email as Email,
                         Congregations.Congregation_Name as Community
 
     from                dbo.Contacts
     join                dbo.Participants
     on                  Contacts.Contact_ID = Participants.Contact_ID
+    join                dbo.dp_Users
+    on                  dp_Users.Contact_ID = Contacts.Contact_ID
     left join           dbo.Households
     on                  Households.Household_ID = Contacts.Household_ID
     left join           dbo.Congregations
     on                  Congregations.Congregation_ID = Households.Congregation_ID
-    where               (Contacts.__Age > 12 OR Contacts.__Age is null)
+    where               (Contacts.__Age > 12 or Contacts.__Age is null)
     and                 Contacts.Email_Address is not null
     and                 Participants.Participant_Start_Date > @LastSuccessfulSyncDate

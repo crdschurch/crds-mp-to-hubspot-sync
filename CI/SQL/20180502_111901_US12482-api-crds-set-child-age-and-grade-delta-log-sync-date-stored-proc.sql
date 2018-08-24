a32dda38efe475181525b75a71de05bdb3361cbe
agause <agause@callibrity.com>
2018-08-24 15:43:06 -0400

    - Attributes Kids Club & Student Ministry child age/grade contact data to "Other Household(s)" (Contact_Households.Household_ID), in addition to the primary household (Contacts.Household_ID). - Fixes SQL Server datetime (persisted as UTC) --> .NET DateTime object conversion where the value from the db becomes a DateTime.Kind of "Unspecified". Fixes by casting the datetime instance to a type of datetimeoffset, which assumes the .NET DateTime.Kind of "Utc".

--- CI/SQL/20180502_111901_US12482-api-crds-set-child-age-and-grade-delta-log-sync-date-stored-proc.sql
+++ CI/SQL/20180502_111901_US12482-api-crds-set-child-age-and-grade-delta-log-sync-date-stored-proc.sql
@@ -7,19 +7,31 @@ create or alter procedure dbo.api_crds_set_child_age_and_grade_delta_log_sync_da
 as
 
     if exists (select 1 from dbo.cr_ChildAgeAndGradeDeltaLog where ProcessedUtc = (select max(ProcessedUtc) from dbo.cr_ChildAgeAndGradeDeltaLog where SyncCompletedUtc is null))
-    begin
+        begin
 
-        declare @SyncCompletedUtc datetime = getutcdate();
+            declare @SyncCompletedUtc datetime = getutcdate();
 
-        update      cr_ChildAgeAndGradeDeltaLog
-        set         SyncCompletedUtc = @SyncCompletedUtc,
-                    LastModifiedUtc = @SyncCompletedUtc
+            update      cr_ChildAgeAndGradeDeltaLog
+            set         SyncCompletedUtc = @SyncCompletedUtc,
+                        LastModifiedUtc = @SyncCompletedUtc
+
+            from        dbo.cr_ChildAgeAndGradeDeltaLog
+            where       ProcessedUtc = (select max(ProcessedUtc) from dbo.cr_ChildAgeAndGradeDeltaLog where SyncCompletedUtc is null);
+
+            select      cast(ProcessedUtc as datetimeoffset) ProcessedUtc,
+                        cast(SyncCompletedUtc as datetimeoffset) SyncCompletedUtc,
+                        InsertCount,
+                        UpdateCount
+
+            from        dbo.cr_ChildAgeAndGradeDeltaLog
+            where       SyncCompletedUtc = @SyncCompletedUtc;
+
+        end
+    else
+        select      cast(ProcessedUtc as datetimeoffset) ProcessedUtc,
+                    cast(SyncCompletedUtc as datetimeoffset) SyncCompletedUtc,
+                    InsertCount,
+                    UpdateCount
 
         from        dbo.cr_ChildAgeAndGradeDeltaLog
-        where       ProcessedUtc = (select max(ProcessedUtc) from dbo.cr_ChildAgeAndGradeDeltaLog where SyncCompletedUtc is null);
-
-        select ProcessedUtc, SyncCompletedUtc, InsertCount, UpdateCount from dbo.cr_ChildAgeAndGradeDeltaLog where SyncCompletedUtc = @SyncCompletedUtc;
-
-    end
-
-    select ProcessedUtc, SyncCompletedUtc, InsertCount, UpdateCount from dbo.cr_ChildAgeAndGradeDeltaLog where ProcessedUtc = (select max(ProcessedUtc) from dbo.cr_ChildAgeAndGradeDeltaLog);
+        where       ProcessedUtc = (select max(ProcessedUtc) from dbo.cr_ChildAgeAndGradeDeltaLog);

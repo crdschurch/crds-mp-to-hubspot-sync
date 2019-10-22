5d04b7ca92ab8e9be5f29d183bdb8662a58903e5
Jason Brammer <brammer.jason@gmail.com>
2019-10-22 16:18:47 -0400

    reset whitespacing

--- Crossroads.Service.HubSpot.Sync.ApplicationServices/Services/Impl/CreateOrUpdateContactsInHubSpot.cs
+++ Crossroads.Service.HubSpot.Sync.ApplicationServices/Services/Impl/CreateOrUpdateContactsInHubSpot.cs
@@ -107,6 +107,7 @@ More details will be available in the serial processing logs.");
                 {
                     var contact = hubSpotContacts[currentContactIndex];
                     var response = _http.Post($"contacts/v1/contact?hapikey={_hubSpotApiKey}", contact);
+                    
                     switch (response.StatusCode)
                     {
                         case HttpStatusCode.OK: // 200; create endpoint
